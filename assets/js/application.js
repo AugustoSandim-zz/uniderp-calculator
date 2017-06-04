fc542ab4a8bb608a074d800f86be777b973d1d33
Augusto Sandim <augusto@jera.com.br>
2017-06-04 14:28:28 -0400

    Adicionando cor nova e atualizando ações ao apagar

--- assets/js/application.js
+++ assets/js/application.js
@@ -2,12 +2,14 @@ document.addEventListener('DOMContentLoaded', function() {
   var digitedNumber = document.querySelectorAll("#digited-number");
   var result = document.querySelectorAll("#result-final");
   var number = digitedNumber[0].children[0];
+  var clearAll = document.querySelectorAll(".clear-all");
   
   [].forEach.call(document.querySelectorAll(".button-number"), function(el) {
     el.addEventListener("click", function() {
       
       number.innerText = (number.innerText + "" + this.value) || this.value;
       result[0].children[0].innerText = CalculatorN2(number.innerText);
+      clearAll[0].style.display = "block"; 
     });
   });
 
@@ -16,6 +18,11 @@ document.addEventListener('DOMContentLoaded', function() {
       
       number.innerText = number.innerText.slice(0, -1);
       result[0].children[0].innerText = CalculatorN2(number.innerText);
+      
+      if(number.innerText.length === 0) {
+        clearAll[0].style.display = "none";
+        result[0].children[0].innerText = "0.00"
+      }
     });
   });
 
